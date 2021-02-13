23d81154ddc0344a15230208323abea73ebd2d76
hectoralarcon <hectoralarconguerra@gmail.com>
2021-02-13 18:57:35 -0500

    Editar turno

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, LogBox } from "react-native";
 import RutasAutenticadas from "./src/Navegacion/RutasAutenticadas";
 import RutasNoAutenticadas from "./src/Navegacion/RutasNoAutenticadas";
+
+LogBox.ignoreLogs(["Setting a timer"]);
 
 export default function App() {
   return <RutasAutenticadas />;
